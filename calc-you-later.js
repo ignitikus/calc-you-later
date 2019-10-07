89a4b0c718397680a8bb7eaefc7c097e4143fd58
NikolayK <nikolay.kim@codeimmersive.com>
2019-10-07 14:11:32 -0400

    adds solutions

--- calc-you-later.js
+++ calc-you-later.js
@@ -1,1 +1,7 @@
-const calculate = require('./calculate')
+const calculate = require('./calculate');
+
+const num1 = process.argv[2];
+const sign = process.argv[4];
+const num2 = process.argv[3];
+
+console.log(calculate)
--- calculate.js
+++ calculate.js
@@ -1,5 +1,18 @@
-function calculate() {
-  
+
+function calculate(num1, num2, sign) {    if(sign === '+' || sign.toLowerCase() === "plus") {
+        return parseFloat(num1) + parseFloat(num2);
+    } else if (sign === '-' || sign.toLowerCase() === "minus" ){
+        return parseFloat(num1) - parseFloat(num2);
+    } else if (sign.toLowerCase() === 'x' || sign.toLowerCase() === "x" || sign.toLowerCase() === "times"){
+        return parseFloat(num1) * parseFloat(num2);
+    } else if (sign === "/"){
+        return parseFloat(num1) / parseFloat(num2);
+    } else if (sign === "%" || sign.toLowerCase() === "modulus"|| sign.toLowerCase() === "mod"){
+        return parseFloat(num1) % parseFloat(num2);
+    }
+    else{
+        return "Sorry, that's not a mathematical operation!"
+    }
 }
 
 
